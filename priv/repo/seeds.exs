044a577898ef4cfc3a14639a78724af7ffd219db
PadmaJS <padshak230@gmail.com>
2022-06-05 08:49:25 +0545

    added seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ApiLearning.{Repo, Accounts.User}
+
+email = "apitest@example.com"
+password = "1234567"
+
+Repo.insert!(%User{email: email, password: password})
